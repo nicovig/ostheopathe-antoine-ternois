36f0b91936d793ddda8414a3d7a0929e67638614
nico <koalaviril@gmail.com>
2019-11-21 12:16:54 +0100

    feat: config form

--- src/Form/AppointmentType.php
+++ src/Form/AppointmentType.php
@@ -3,24 +3,56 @@
 namespace App\Form;
 
 use App\Entity\Appointment;
+use App\Entity\Session;
+use App\Entity\Location;
+use App\Entity\Practitioner;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Repository\PractitionerRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AppointmentType extends AbstractType
 {
+    private $om;
+
+    public function __construct(ObjectManager $objectManager)
+    {
+        $this->om = $objectManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date')
-            ->add('reason')
-            ->add('review')
-            ->add('realTime')
-            ->add('createdAt')
-            ->add('isActive')
-            ->add('session')
-            ->add('location')
-            ->add('prescription')
+            ->add('reason', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Motif de la consultation'
+                ]
+            ])
+
+            ->add('session', EntityType::class,[
+                'class' => Session::class,
+                'choice_label' => 'type',
+            ])
+
+            ->add('location', EntityType::class,[
+                'class' => Location::class,
+                'choice_label' => 'city',
+            ])
+
+            ->add('practitioner', EntityType::class,[
+                'class' => Practitioner::class,
+                'em' =>
+                'choice_label' => 'id',
+            ])
+            
+
+            ->add('save', SubmitType::class, [
+                'label' => 'Valider mon rendez-vous'
+            ])
         ;
     }
 
